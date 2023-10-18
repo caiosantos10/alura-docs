a96159a3ec0bfec722b5d4c705fa10ca31eaccda
Caio Santos <caiosant10@gmail.com>
2023-10-18 15:50:12 -0300

    iniciando telas com valores no campo

--- public/documento.js
+++ public/documento.js
@@ -1,9 +1,24 @@
 const socket = io();
 
+const params = new URLSearchParams(window.location.search);
+const documentName = params.get('nome');
+
 const input = document.getElementById('editor-texto');
+const documentTitle = document.getElementById('titulo-documento');
+
+documentTitle.textContent = documentName || 'Documento sem título';
+
+function selectDocument(name) {
+    socket.emit('select-document', name);
+}
+selectDocument(documentName);
 
 input.addEventListener('keyup', () => {
-    socket.emit('input-text', input.value);
+    socket.emit('input-text', input.value, documentName);
+});
+
+socket.on('document-text', (text) => {
+    input.value = text;
 });
 
 socket.on('input-text', (text) => {
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,8 +1,36 @@
 import io from './server.js';
 
+const docs = [
+    {
+        name: 'JavaScript',
+        text: 'texto de javascript'
+    },
+    {
+        name: 'Node',
+        text: 'texto de nodejs'
+    },
+    {
+        name: 'Socket.io',
+        text: 'texto de socket'
+    }
+]
+
 io.on('connection', (socket) => {
-    console.log('Um clinte se conectou, ID: ', socket.id)
-    socket.on('input-text', (value) => {
-        socket.broadcast.emit('input-text', value);
+    console.log('Um clinte se conectou, ID: ', socket.id);
+
+    socket.on('select-document', (documentName) => {
+        socket.join(documentName);
+        const document = findDocument(documentName);
+
+        if (document) {
+            socket.emit('document-text', document.text);
+        }
+    });
+    socket.on('input-text', (value, documentName) => {
+        socket.to(documentName).emit('input-text', value);
     });
 });
+
+function findDocument(name) {
+    return docs.find(document => document.name === name);
+}
